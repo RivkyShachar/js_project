3cb326a1510264156cc2f09b38e76f97d80f8d6c
RivkyShachar <rvider@g.jct.ac.il>
2023-11-15 19:41:14 +0200

    need to do borders

--- js/county.js
+++ js/county.js
@@ -11,40 +11,64 @@ export default class County{
         this.flag = _item.flags.png;
         this.latlng = _item.latlng;
         this.area = Number(_item.area).toLocaleString();
-        _item.hasOwnProperty('borders')? this.borders = _item.borders : this.borders = ["No borders"];
-        this.joinBorders = this.borders==null? "": this.borders.join(', ');
+        this.borders = _item.borders || [];
+        this.joinBorders = this.borders.length > 0 ? this.borders.map(border => `<a href="#" class="border-link" data-border="${border}">${border}</a>`).join(', ') : "No borders";
         console.log(this.languages);
     }
     render = () => {
         let parent1 = document.querySelector(this.parent);
 
         // Convert each border to a link
-        const borderLinks = this.borders.map(border => `<a href="https://restcountries.com/v3.1/alpha/${border}" target="_blank">${border}</a>`);
+        // const borderLinks = this.borders.map(border => `<a href="https://restcountries.com/v3.1/alpha/${border}" target="_blank">${border}</a>`);
 
+        // parent1.innerHTML += `
+        //     <div class="card mt-4">
+        //         <div class="row g-0">
+        //             <div class="col-md-2">
+        //                 <img src="${this.flag}" class="card-img" alt="${this.name} Flag">
+        //                 <p>Borders: ${borderLinks.join(', ')}</p>
+        //             </div>
+        //             <div class="col-md-10">
+        //                 <div class="card-body">
+        //                     <h5 class="card-title">${this.name}</h5>
+        //                     <p class="card-text">Population: ${this.pop}</p>
+        //                     <p class="card-text">Region: ${this.region}</p>
+        //                     <p class="card-text">Languages: ${this.joinLanguages}</p>
+        //                     <p class="card-text">Coins: ${this.renderCoin(this.coin)}</p>
+        //                     <p class="card-text">Capital: ${this.capital}</p>
+        //                     <p class="card-text">Area: ${this.area} sq km</p>
+        //                     <iframe class="mt-3" width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+        //                         src="https://maps.google.com/maps?q=${this.latlng[0]},${this.latlng[1]}&hl=es&z=14&amp;output=embed">
+        //                     </iframe>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // `;
         parent1.innerHTML += `
-            <div class="card mt-4">
-                <div class="row g-0">
-                    <div class="col-md-2">
-                        <img src="${this.flag}" class="card-img" alt="${this.name} Flag">
-                        <p>Borders: ${borderLinks.join(', ')}</p>
-                    </div>
-                    <div class="col-md-10">
-                        <div class="card-body">
-                            <h5 class="card-title">${this.name}</h5>
-                            <p class="card-text">Population: ${this.pop}</p>
-                            <p class="card-text">Region: ${this.region}</p>
-                            <p class="card-text">Languages: ${this.joinLanguages}</p>
-                            <p class="card-text">Coins: ${this.renderCoin(this.coin)}</p>
-                            <p class="card-text">Capital: ${this.capital}</p>
-                            <p class="card-text">Area: ${this.area} sq km</p>
-                            <iframe class="mt-3" width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-                                src="https://maps.google.com/maps?q=${this.latlng[0]},${this.latlng[1]}&hl=es&z=14&amp;output=embed">
-                            </iframe>
-                        </div>
-                    </div>
+        <div class="card mt-4">
+            <div class="row g-0">
+                <div class="col-md-2">
+                    <img src="${this.flag}" class="card-img" alt="${this.name} Flag">
+                    <p>Borders: ${this.joinBorders}</p>
                 </div>
+                <!-- ... (rest of your existing HTML) -->
             </div>
-        `;
+        </div>
+    `;
+
+    // Add click event listeners to border links
+    this.borders.forEach(border => {
+        const borderLink = document.querySelector(`.border-link[data-border="${border}"]`);
+        if (borderLink) {
+            borderLink.addEventListener('click', (event) => {
+                event.preventDefault();
+                // Call your doApi function with the corresponding border
+                // doApi(border);
+                alert(border)
+            });
+        }
+    });
     }
 
     renderCoin = (currencies) => {
