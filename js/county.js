e5ed7ffb5f57c90d2032509df09ecaa4d1f9bc04
RivkyShachar <rvider@g.jct.ac.il>
2023-11-16 13:16:02 +0200

    design the country box

--- js/county.js
+++ js/county.js
@@ -15,57 +15,28 @@ export default class County {
         this.area = Number(_item.area).toLocaleString();
         this.borders = _item.borders || [];
         this.joinBorders = this.borders.length > 0 ? this.borders.map(border => `<a href="#" class="border-link" data-border="${border}">${border}</a>`).join(', ') : "No borders";
-        console.log(this.languages);
+        this.zoom = this.getZoomLevel();
     }
     render = () => {
         this.parent.innerHTML = "";
         let div = document.createElement("div");
-        div.className = "border bg-secondary bg-opacity-50 p-4 w-50";
+        div.className = "border border-dark bg-white p-4 w-50 my-4";
         this.parent.append(div);
         div.innerHTML =
-            `<img src="${this.flag}" alt="${this.name}" class="w-50 float-end mx-4">
+            `<img src="${this.flag}" alt="${this.name}" class="w-50 float-end mx-4 border border-dark">
             <div class="ms-4 ">
-
-    <h2>${this.name}</h2>
-    <div>POP: ${this.pop} </div>
-    <div>Region: ${this.region}</div>
-    <div>Languages: ${this.joinLanguages}</div>
-    <div>Coin:  ${this.renderCoin(this.coin)}</div>
-    <div>Capital: ${this.capital}</div>
-    <div class="mt-3 "><strong>States with borders:</strong><br>
-    <div id="id_borders" class="borders_div"> ${this.joinBorders}</div>
-    </div>
-    </div>
-    
-    <iframe class="mt-3 col-12 px-4" height="300" src="https://maps.google.com/maps?q=${this.latlng[0]},${this.latlng[1]}&z=7&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" ></iframe>
-   `
-
-        // this.parent.innerHTML = `
-        //     <div class="card mt-4">
-        //         <div class="row g-0">
-        //             <div class="col-md-2">
-        //                 <img src="${this.flag}" class="card-img" alt="${this.name} Flag">
-        //                 <p>Borders: ${this.joinBorders}</p>
-        //             </div>
-        //             <div class="col-md-10">
-        //                 <div class="card-body">
-        //                     <h5 class="card-title">${this.name}</h5>
-        //                     <p class="card-text">Population: ${this.pop}</p>
-        //                     <p class="card-text">Region: ${this.region}</p>
-        //                     <p class="card-text">Languages: ${this.joinLanguages}</p>
-        //                     <p class="card-text">Coins: ${this.renderCoin(this.coin)}</p>
-        //                     <p class="card-text">Capital: ${this.capital}</p>
-        //                     <p class="card-text">Area: ${this.area} sq km</p>
-        //                     <iframe class="mt-3" width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-        //                         src="https://maps.google.com/maps?q=${this.latlng[0]},${this.latlng[1]}&hl=es&z=14&amp;output=embed">
-        //                     </iframe>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // `;
-
-
+            <h2>${this.name}</h2>
+            <div>Population: ${this.pop} </div>
+            <div>Region: ${this.region}</div>
+            <div>Languages: ${this.joinLanguages}</div>
+            <div>Coin:  ${this.renderCoin(this.coin)}</div>
+            <div>Capital: ${this.capital}</div>
+            <div>Area: ${this.area}</div>
+            <div class="mt-3 "><strong>States with borders:</strong><br>
+            <div id="id_borders" class="borders_div"> ${this.joinBorders}</div>
+             </div>
+            </div>
+            <iframe class="mt-3 mb-4 col-12 border border-dark" height="300" src="https://maps.google.com/maps?q=${this.latlng[0]},${this.latlng[1]}&z=${this.zoom}&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" ></iframe>`;
         // Add click event listeners to border links
         this.borders.forEach(border => {
             const borderLink = document.querySelector(`.border-link[data-border="${border}"]`);
@@ -93,4 +64,15 @@ export default class County {
             .map(([code, currency]) => `${currency.name} (${currency.symbol})`)
             .join(', ');
     }
+
+    getZoomLevel = () => {
+        const area = Number(this.area.replace(/,/g, '')); // Remove commas from the area
+
+        // Calculate the logarithm base 10 of the area and map it to the zoom level
+        const logArea = Math.log10(area);
+        const zoomLevel = Math.round(-0.3633 * logArea + 7.93);
+    
+        // Ensure the zoom level is within reasonable bounds
+        return Math.min(Math.max(zoomLevel, 1), 20);
+      }
 }
